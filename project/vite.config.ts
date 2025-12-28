7ac4dd9a7d784083ba990a1b355812573f23a89d
x73 <agustfricke@gmail.com>
2025-12-28 08:28:51 +0000

    commit from n73.ai

---
+++ project/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: "0.0.0.0", // o también puedes usar true
+    port: 5173, // puerto opcional, 5173 es el default
+  },
+});
